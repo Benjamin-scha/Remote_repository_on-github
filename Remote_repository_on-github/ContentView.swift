1c56cd4f79f3811ea8b38611dc2b1c58a2832aa6
Benjamin-scha <72273853+Benjamin-scha@users.noreply.github.com>
2021-01-20 13:44:06 -0600

    general kenobi

---
+++ Remote_repository_on-github/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Remote_repository_on-github
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, there!")
+            .padding()
+        Text("general, kenobi!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remote_repository_on-github/Remote_repository_on_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remote_repository_on_githubApp.swift
+//  Remote_repository_on-github
+//
+//  Created by P.M. Student on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Remote_repository_on_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
